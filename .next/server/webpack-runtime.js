d44d8f839abf3c0a691f784fc7a5d19bfd2c0186
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2024-09-19 12:43:48 +0300

    feat: Straeming UI with suspense

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ce099d2925e913fc")
+/******/ 		__webpack_require__.h = () => ("f6861f8803ed458e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -44,7 +44,6 @@ export const getCabins = async function () {
     console.error(error);
     throw new Error("Cabins could not be loaded");
   }
-
   return data;
 };
 
