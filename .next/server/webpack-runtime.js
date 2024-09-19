69d1189abdb263bac89acdcd533afc12413e43ab
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2024-09-19 12:31:54 +0300

    refact: refactoring Cabin List into a new component

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("eae2ca78e8940743")
+/******/ 		__webpack_require__.h = () => ("ce099d2925e913fc")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,11 @@
-import Counter from "@/app/_components/Counter";
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import CabinList from "../_components/CabinList";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+export default function Page() {
   // CHANGE
-  const cabins = await getCabins();
 
   return (
     <div>
@@ -23,14 +20,7 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <CabinList />
     </div>
   );
 }
