412220f3866160317710849cd32184dab1c8dc0b
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2024-09-26 08:36:42 +0300

    Server components in client components

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1trF7wy7WagaIPnx5WNs9GIDshXGuEXMmn3nc+KCnj4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pgbjT+6y9TA6WpBdDiqWsbHk9r5jxjvgAXg8I0mO+nk=\"\n}"
